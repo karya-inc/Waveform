df53e83aa2a3f54b49da1a332662af09c054383a
Divyansh Kushwaha <thedroiddiv@gmail.com>
2025-10-22 13:44:29 +0530

    create a center pinned waveform with zoom-in capability

---
+++ app/src/main/java/com/daiatech/waveform/segmentation/AudioSegmentPicker.android.kt
@@ -0,0 +1,27 @@
+package com.daiatech.waveform.segmentation
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import com.daiatech.waveform.models.waveformColors
+
+@Preview
+@Composable
+private fun AudioSegmentPickerPrev1Android() {
+    AudioSegmentPickerPreview()
+}
+
+@Preview
+@Composable
+private fun AudioSegmentPickerPreview2Android() {
+    AudioSegmentPickerPreview(
+        colors = waveformColors(
+            containerColor = Color(0xFF001F24),
+            markerColor = Color(0xFF008857),
+            waveformColor = Color(0xFFD5D5D5),
+            primaryProgressColor = Color(0xFFDE3730),
+            fadeColor = Color(0xFF00363D),
+            buttonColor = Color(0xFF008857)
+        )
+    )
+}
