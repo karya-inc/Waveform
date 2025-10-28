40d583b3aca5df8c578960a62ce706630ea6983e
Divyansh Kushwaha <thedroiddiv@gmail.com>
2025-10-28 16:46:02 +0530

    create enums for zoom and playback speed

---
+++ waveform/src/commonMain/kotlin/com/daiatech/waveform/segmentation/models/PlaybackSpeed.kt
@@ -0,0 +1,7 @@
+package com.daiatech.waveform.segmentation.models
+
+enum class PlaybackSpeed(val value: Float) {
+    X0_25(0.25F),
+    X0_50(0.50F),
+    X1_00(1.00F),
+}
---
+++ waveform/src/commonMain/kotlin/com/daiatech/waveform/segmentation/models/Zoom.kt
@@ -0,0 +1,9 @@
+package com.daiatech.waveform.segmentation.models
+
+enum class Zoom(val value: Int) {
+    X1(1),
+    X2(2),
+    X3(3),
+    X4(4),
+    X5(5)
+}
